20505047d764cfdb3264ffb35578b0075d10058e
Flavia Bojescu <flaviabojescu@gmail.com>
2019-09-04 15:44:02 +0100

    hashes without errors

--- directory.rb
+++ directory.rb
@@ -1,16 +1,16 @@
 #first we print the list of students
 students = [
-{name: "Dr. Hannibal Lecter", cohort:november},
-{name: "Darth Vader", cohort:november},
-{name: "Nurse Ratched", cohort:november},
-{name: "Michael Corleone", cohort:november},
-{name: "Alex DeLarge", cohort:november},
-{name: "The Wicked Witch of the West", cohort:november},
-{name: "Terminator", cohort:november},
-{name: "Freddy Krueger", cohort:november},
-{name: "The Joker", cohort:november},
-{name: "Joffrey Baratheon", cohort:november},
-{name: "Norman Bates", cohort:november}
+{name: "Dr. Hannibal Lecter", cohort: :november},
+{name: "Darth Vader", cohort: :november},
+{name: "Nurse Ratched", cohort: :november},
+{name: "Michael Corleone", cohort: :november},
+{name: "Alex DeLarge", cohort: :november},
+{name: "The Wicked Witch of the West", cohort: :november},
+{name: "Terminator", cohort: :november},
+{name: "Freddy Krueger", cohort: :november},
+{name: "The Joker", cohort: :november},
+{name: "Joffrey Baratheon", cohort: :november},
+{name: "Norman Bates", cohort: :november}
 ]
 def print_header
   puts "The students of Villains Academy"
